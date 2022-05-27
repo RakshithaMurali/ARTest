93e1f1ac492548a7fca958394039d12483564f8b
RakshithaMurali <rakshithamurali8@gmail.com>
2022-05-28 00:48:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-       <button onClick={() => window.app.showChair(`./knight2.glb`)}>AR Button</button> 
+       <button onClick={() => window.app.showChair(`https://firebasestorage.googleapis.com/v0/b/arrange-eafdc.appspot.com/o/0wTemIueqT%2Fscene.glb?alt=media&token=141c1b57-21ad-4047-aef1-ce01accbffcd`)}>AR Button</button> 
     </div>
   );
 }
