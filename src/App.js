335b6ef8da9d8c30c2be59771cc56dca9e2eaac8
RakshithaMurali <rakshithamurali8@gmail.com>
2022-05-27 18:19:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,18 +4,11 @@ import { ARApp } from './complete/lecture3_11/app';
 
 function App() {
 
-  useEffect(() => {
-    console.log("here")
-    document.addEventListener("div", ()=> {
-      console.log("Div")
-    } )
     document.addEventListener("DOMContentLoaded", ()=>{
       const app = new ARApp();
       window.app = app;
       console.log(window.app)
   });
-
-  },[]);
 
   return (
     <div className="App">
